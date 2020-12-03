f7cb5fc18885947b9a1ec9d716a094d70fa3c290
leon-taquet <taquet.leon@gmail.com>
2020-12-02 23:53:38 -0500

    Removed Bitpack

--- parse.c
+++ parse.c
@@ -3,8 +3,14 @@
 #include <stdlib.h>
 #include <bitpack.h>
 
-
 #define LENGTH_HINT 100
+
+static const uint32_t MASK_VALUE = (~(uint32_t)0 >> 7);
+static const uint32_t MASK_REG = ~(~(uint32_t)0 << 3);  
+static const uint32_t MASK_RC = MASK_REG;
+static const uint32_t MASK_RB = MASK_RC << 3;
+static const uint32_t MASK_RA = MASK_RB << 3; 
+
 
 //Seq_T parse_instructions(FILE* fp) 
 seg_t *parse_instructions(FILE* fp) 
@@ -51,15 +57,19 @@ seg_t *parse_instructions(FILE* fp)
 
 Um_opcode parse_opcode(uint32_t instruction)
 {
-    return (Um_opcode)Bitpack_getu((uint64_t)instruction, 4, 28);
+    return (Um_opcode)(instruction >> 28);
+    //return (Um_opcode)Bitpack_getu((uint64_t)instruction, 4, 28);
 }
 
 three_register parse_three_register(uint32_t instruction) 
 {
     three_register result;
-    result.ra = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 6);
-    result.rb = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 3);
-    result.rc = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 0);
+    /* result.ra = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 6); */
+    /* result.rb = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 3); */
+    /* result.rc = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 0); */
+    result.ra = (instruction & MASK_RA) >> 6;
+    result.rb = (instruction & MASK_RB) >> 3;
+    result.rc = (instruction & MASK_RC);
 
     return result;
 }
@@ -67,8 +77,10 @@ three_register parse_three_register(uint32_t instruction)
 load_value parse_load_value(uint32_t instruction)
 {
     load_value result;
-    result.value = (uint32_t)Bitpack_getu((uint64_t)instruction, 25, 0);
-    result.r = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 25);
+    //result.value = (uint32_t)Bitpack_getu((uint64_t)instruction, 25, 0);
+    result.value = instruction & MASK_VALUE;
+    result.r = (instruction >> 25) & MASK_REG;
+    //result.r = (uint32_t)Bitpack_getu((uint64_t)instruction, 3, 25);
 
     return result;
 }
--- um.c
+++ um.c
@@ -6,66 +6,66 @@
 #include "mainmem.h"
 #include "machine.h"
 
-void test_parse_instructions(FILE* fp) 
-{
-    Seq_T instructions = parse_instructions(fp);
+/* void test_parse_instructions(FILE* fp)  */
+/* { */
+/*     Seq_T instructions = parse_instructions(fp); */
 
-    for (int i = 0; i < Seq_length(instructions); ++i) {
-        fprintf(stdout, "%x", (uint32_t)(uintptr_t)Seq_get(instructions, i));
-    }
+/*     for (int i = 0; i < Seq_length(instructions); ++i) { */
+/*         fprintf(stdout, "%x", (uint32_t)(uintptr_t)Seq_get(instructions, i)); */
+/*     } */
 
-    Seq_free(&instructions);
-}
+/*     Seq_free(&instructions); */
+/* } */
 
-void test_parse_opcode(FILE* fp)
-{
-    Seq_T instructions = parse_instructions(fp);
+/* void test_parse_opcode(FILE* fp) */
+/* { */
+/*     Seq_T instructions = parse_instructions(fp); */
 
-    for (int i = 0; i < Seq_length(instructions); ++i) {
-        fprintf(stdout, "%d\n", 
-            parse_opcode((uint32_t)(uintptr_t)Seq_get(instructions, i)));
-    }
+/*     for (int i = 0; i < Seq_length(instructions); ++i) { */
+/*         fprintf(stdout, "%d\n",  */
+/*             parse_opcode((uint32_t)(uintptr_t)Seq_get(instructions, i))); */
+/*     } */
 
-    Seq_free(&instructions);
-}
+/*     Seq_free(&instructions); */
+/* } */
 
-void test_three_register(FILE* fp)
-{
-    Seq_T instructions = parse_instructions(fp);
+/* void test_three_register(FILE* fp) */
+/* { */
+/*     Seq_T instructions = parse_instructions(fp); */
 
-    for (int i = 0; i < Seq_length(instructions); ++i) {
-        three_register t = parse_three_register(
-            (uint32_t)(uintptr_t)Seq_get(instructions, i)
-        );
-        fprintf(stdout, "ra: %d rb: %d rc: %d\n", 
-            t.ra, t.rb, t.rc);
-    }
+/*     for (int i = 0; i < Seq_length(instructions); ++i) { */
+/*         three_register t = parse_three_register( */
+/*             (uint32_t)(uintptr_t)Seq_get(instructions, i) */
+/*         ); */
+/*         fprintf(stdout, "ra: %d rb: %d rc: %d\n",  */
+/*             t.ra, t.rb, t.rc); */
+/*     } */
 
-    Seq_free(&instructions);
-}
+/*     Seq_free(&instructions); */
+/* } */
 
-void test_load_value(FILE* fp)
-{
-    Seq_T instructions = parse_instructions(fp);
+/* void test_load_value(FILE* fp) */
+/* { */
+/*     Seq_T instructions = parse_instructions(fp); */
 
-    for (int i = 0; i < Seq_length(instructions); ++i) {
-        load_value l = parse_load_value(
-            (uint32_t)(uintptr_t)Seq_get(instructions, i)
-        );
-        fprintf(stdout, "r: %d value: %d\n", 
-            l.r, l.value);
-    }
+/*     for (int i = 0; i < Seq_length(instructions); ++i) { */
+/*         load_value l = parse_load_value( */
+/*             (uint32_t)(uintptr_t)Seq_get(instructions, i) */
+/*         ); */
+/*         fprintf(stdout, "r: %d value: %d\n",  */
+/*             l.r, l.value); */
+/*     } */
 
-    Seq_free(&instructions);
-}
+/*     Seq_free(&instructions); */
+/* } */
 
-void test_mem_newfree(FILE* fp)
-{
-    MainMem mem = MainMem_new(parse_instructions(fp));
+/* void test_mem_newfree(FILE* fp) */
+/* { */
+/*     MainMem mem = MainMem_new(parse_instructions(fp)); */
 
-    MainMem_free(&mem);
-    assert(mem == NULL);
-}
+/*     MainMem_free(&mem); */
+/*     assert(mem == NULL); */
+/* } */
 
 int main(int argc, char** argv) 
 {
